98f1d73cc96b0a7b50153218d688519984637744
Hoang Pham <hoangpm6814@gmail.com>
2019-12-06 08:54:19 +0700

    Đăng ký, đăng nhập
    Đăng ký, đăng nhập thành công

---
+++ News App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by I Am Focused on 11/29/19.
+//  Copyright © 2019 LVHhcmus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
