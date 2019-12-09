a1c81b575a9975ecb6dbad2021e92c6e3f2263ac
Hang Thanh <hangthanh180599@gmail.com>
2019-12-10 00:22:43 +0700

    highlight selected text in text view

--- News App/NewsDetailViewController.swift
+++ News App/NewsDetailViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class NewsDetailViewController: UIViewController {
+class NewsDetailViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var newsContentTV: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        customMenuSelectedText()
+    }
+    
+    func customMenuSelectedText() {
+        let highlightItem = UIMenuItem(title: "Highlight", action: #selector(highlightSelectedText))
+        let translateItem = UIMenuItem(title: "Translate", action: #selector(translateSelectedText))
+        UIMenuController.shared.menuItems = [highlightItem, translateItem]
+    }
+    
+    @objc func highlightSelectedText() {
+        if let range = newsContentTV.selectedTextRange, let selectedText = newsContentTV.text(in: range) {
+            let string = NSMutableAttributedString(attributedString: newsContentTV.attributedText)
+            let attributes = [NSAttributedString.Key.backgroundColor: UIColor.yellow]
+            string.addAttributes(attributes, range: newsContentTV.selectedRange)
+            newsContentTV.attributedText = string
+           print(selectedText)
+            //send data to highlight list
+            //do something here
+        }
+    }
+    
+    @objc func translateSelectedText() {
+        if let range = newsContentTV.selectedTextRange, let selectedText = newsContentTV.text(in: range) {
+           print(selectedText)
+            print("Translated")
+        }
     }
     
 
